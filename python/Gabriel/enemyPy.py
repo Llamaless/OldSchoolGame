f2372d91387867d164c50b595fae7ce4c948f64b
gdumitru <gabby_dumitru@yahoo.com>
2017-02-13 09:55:54 +0000

    EnemyPy

---
+++ python/Gabriel/enemyPy.py
@@ -0,0 +1,40 @@
+class Enemy:
+
+    _enemynr = 0
+
+    def __init__(self, name, area, level, damage, difficulty, xp, hp):
+
+        self.name = name
+        self.area = area
+        self.level = level
+        self.damage = damage
+        self.difficulty = difficulty
+        self.xp = xp
+        self.hp = hp
+        
+    def getenemyLevel(level,area):
+        
+        return level
+
+    def getenemyName(name):
+        return name
+
+    def getenemyArea(area):
+        return area
+
+    def getenemyDamage(damage):
+        return damage
+
+    def getenemyDifficulty(difficulty):
+        return difficulty
+
+    def getenemyXp(xp):
+        return xp
+
+    def getenemyHp(hp):
+        return hp
+    
+
+        
+
+        
