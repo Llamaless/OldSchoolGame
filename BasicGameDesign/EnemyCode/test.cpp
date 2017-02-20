554f957783f27b6dafcc1adb739929b65a4816ac
gdumitru <gabby_dumitru@yahoo.com>
2017-02-20 15:35:26 +0000

    Add files via upload
    Test.cpp for creating an enemy

---
+++ BasicGameDesign/EnemyCode/test.cpp
@@ -0,0 +1,20 @@
+
+// test 
+#include "Enemy.h"
+#include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+
+	Enemy enemy; //creates object
+
+	enemy.cinit(1, "1234", "forest"); //define object using a function
+
+	return 0;
+
+	
+	
+}
